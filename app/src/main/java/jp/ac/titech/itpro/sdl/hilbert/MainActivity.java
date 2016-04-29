7dabacb62e87a9afb2c944e8a4121de8cf88917f
tkntkn613 <dummy@example.com>
2016-04-29 15:52:21 +0900

    output MainActivity's life cycle events

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -4,16 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.util.*;
 
 public class MainActivity extends AppCompatActivity {
     private TextView levelView;
     private HilbertView hilbertView;
+    private static final String TAG = "MainActivity";
     private final static int MAX_N = 9;
     private int n = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
         levelView = (TextView)findViewById(R.id.level_view);
         hilbertView = (HilbertView)findViewById(R.id.hilbert_view);
@@ -22,7 +25,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume");
         display();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 
     public void onClickDec(View v) {
